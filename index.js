8bae602c5717ec2841365cf70bad3fb238f2d801
GrannyYetta <you@example.com>
2022-09-26 13:09:32 +0200

    corrected modules and point 4

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const express = require("express");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '/uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = ".jpg")
-      cb(null, file.fieldname + '-' + uniqueSuffix)}
-  })
+  destination: function (req, file, cb) {
+    cb(null, "/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9) + file.originalname;
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
 const upload = multer({ storage: "/uploads" });
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, '/uploads')
+//     },
+//     filename: function (req, file, cb) {
+//       const uniqueSuffix = ".jpg")
+//       cb(null, file.fieldname + '-' + uniqueSuffix)}
+//   })
+
+// const upload = multer({ storage: "/uploads" });
 
 const app = express();
 const port = 5000;
